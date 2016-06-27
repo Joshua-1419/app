ea03695d8c49b9b2ac7ddfca4d6524b0465d2228
Joshua Alain Barreda Nik <joshuaalain1419b.n@gmail.com>
2016-06-27 11:24:13 -0500

    Respositorio inicial

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Aplicaciones</title>
+</head>
+<body>
+	<h1>pagina principal</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo vel alias eveniet saepe maxime non quis quas dolore fuga, aliquid cumque, quam tenetur eligendi incidunt architecto vitae aut, animi perferendis.</p>
+	<h2></h2>
+	
+</body>
+</html>
